7d80b1569dbe682505e7697284b658be6fca067b
baby bin <binhuiyun@gmail.com>
2024-11-06 00:39:57 -0800

    openFeign client in order service

--- order-service/src/main/java/com/techie/microservices/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/techie/microservices/order/OrderServiceApplication.java
@@ -2,9 +2,11 @@ package com.techie.microservices.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
+@EnableFeignClients
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
--- order-service/src/main/java/com/techie/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/techie/microservices/order/service/OrderService.java
@@ -2,14 +2,13 @@ package com.techie.microservices.order.service;
 
 import com.techie.microservices.order.client.InventoryClient;
 import com.techie.microservices.order.dto.OrderRequest;
-import com.techie.microservices.order.event.OrderPlacedEvent;
+//import com.techie.microservices.order.event.OrderPlacedEvent;
 import com.techie.microservices.order.model.Order;
 import com.techie.microservices.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-
 import java.math.BigDecimal;
 import java.util.UUID;
 
@@ -19,7 +18,7 @@ import java.util.UUID;
 public class OrderService {
     private final OrderRepository orderRepository;
    private final InventoryClient inventoryClient;
-   private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
+  // private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
 
@@ -33,14 +32,14 @@ public class OrderService {
             orderRepository.save(order);
 
             // Send the message to Kafka Topic
-            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
+       //     OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
        //     orderPlacedEvent.setOrderNumber(order.getOrderNumber());
         //     orderPlacedEvent.setEmail(orderRequest.userDetails().email());
 //            orderPlacedEvent.setFirstName(orderRequest.userDetails().firstName());
 //            orderPlacedEvent.setLastName(orderRequest.userDetails().lastName());
-           log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
-             kafkaTemplate.send("order-placed", orderPlacedEvent);
-            log.info("End - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
+        //   log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
+          //   kafkaTemplate.send("order-placed", orderPlacedEvent);
+          //  log.info("End - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
         } else {
             throw new RuntimeException("Product is out of stock" + orderRequest.skuCode());
         }
